445c3913b36a09d9d5c7b89bb4dcb2ab5ec9f0d6
David Karapetyan <david.karapetyan@gmail.com>
2016-07-01 19:45:42 -0400

    write occupancy and all readings to dataframe every 15 minutes

--- src/wifiscan.py
+++ src/wifiscan.py
@@ -1,18 +1,45 @@
+import threading
+
 from scapy.layers.dot11 import Dot11
 from scapy.sendrecv import sniff
+import pandas as pd
 
 PROBE_REQUEST_TYPE = 0
 PROBE_REQUEST_SUBTYPE = 4
-ap_list = []
+base_df = pd.DataFrame()
+occupancy_series = pd.Series()
 
 
 def packet_handler(pkt):
+    now = pd.Timestamp.utcnow()
     if pkt.haslayer(Dot11):
         if pkt.type == PROBE_REQUEST_TYPE and pkt.subtype == \
                 PROBE_REQUEST_SUBTYPE:
-            if pkt.addr2 not in ap_list:
-                ap_list.append(pkt.addr2)
-                print("AP MAC: %s with SSID: %s " % (pkt.addr2, pkt.info))
+            new_info_df = pd.DataFrame(
+                data={'addr': pkt.addr2, 'info': pkt.info},
+                index=now)
+            base_df.append(new_info_df)
+            new_occ_ts = pd.Series(data=occupancy_counter(base_df), index=now)
+            occupancy_series.append(new_occ_ts)
+            write_csvs()
+
+            print("AP MAC: %s with SSID: %s " % (pkt.addr2, pkt.info))
+
+
+def occupancy_counter(df):
+    now = pd.Timestamp.utcnow()
+    df_subset = df.between_time(now - pd.Timedelta('15 min'))
+    df_subset.drop_duplicates(inplace=True, keep=last)
+    return len(df_subset)
+
+
+def write_csvs():
+    threading.Timer(900, things_to_be_written)
+
+
+def things_to_be_written():
+    base_df.to_csv("all_info.csv")
+    occupancy_series.to_csv("occupancy.csv")
 
 
 def main():
