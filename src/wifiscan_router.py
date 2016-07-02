445e46418e2cdfe1a624ea91c0197d27298aaace
David Karapetyan <david.karapetyan@gmail.com>
2016-07-02 10:20:20 -0400

    updated for sniffing and writing to json on router

--- src/wifiscan_router.py
+++ src/wifiscan_router.py
@@ -1,45 +1,34 @@
 import threading
+import json
+import datetime
 
 from scapy.layers.dot11 import Dot11
 from scapy.sendrecv import sniff
-import pandas as pd
 
 PROBE_REQUEST_TYPE = 0
 PROBE_REQUEST_SUBTYPE = 4
-base_df = pd.DataFrame()
-occupancy_series = pd.Series()
+base_dict = {}
 
 
 def packet_handler(pkt):
-    now = pd.Timestamp.utcnow()
+    now = datetime.datetime.utcnow()
     if pkt.haslayer(Dot11):
         if pkt.type == PROBE_REQUEST_TYPE and pkt.subtype == \
                 PROBE_REQUEST_SUBTYPE:
-            new_info_df = pd.DataFrame(
-                data={'addr': pkt.addr2, 'info': pkt.info},
-                index=now)
-            base_df.append(new_info_df)
-            new_occ_ts = pd.Series(data=occupancy_counter(base_df), index=now)
-            occupancy_series.append(new_occ_ts)
-            write_csvs()
+            new_info_dict = {'index': now, 'addr': pkt.addr2, 'info': pkt.info}
+            base_dict.update(new_info_dict)
+            write_dicts()
 
             print("AP MAC: %s with SSID: %s " % (pkt.addr2, pkt.info))
 
 
-def occupancy_counter(df):
-    now = pd.Timestamp.utcnow()
-    df_subset = df.between_time(now - pd.Timedelta('15 min'))
-    df_subset.drop_duplicates(inplace=True, keep=last)
-    return len(df_subset)
-
-
-def write_csvs():
+def write_dicts():
     threading.Timer(900, things_to_be_written)
 
 
 def things_to_be_written():
-    base_df.to_csv("all_info.csv")
-    occupancy_series.to_csv("occupancy.csv")
+    with open("all_info.csv", 'w') as file:
+        json.dump(base_dict, file)
 
 
 def main():
