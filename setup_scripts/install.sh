e94730b884b79e05f8178e4ff95d72468c169969
David Karapetyan <david.karapetyan@gmail.com>
2016-07-09 10:59:55 -0400

    updated requirements file and install script (right now, everything isntalled from debian)

--- setup_scripts/install.sh
+++ setup_scripts/install.sh
@@ -1,10 +1,2 @@
-opkg update
-opkg install kmod-usb-uhci
-insmod usbcore
-insmod uhci
-opkg install kmod-usb-ohci
-insmod usb-ohci
-
-#for recognizing ext4 (flash drive is partitioned as this)
-opkg update
-opkg install block-mount kmod-fs-ext4 
+#!/usr/bin/env bash
+sudo apt-get install python-pip gnuplot-x11 python-scapy python-pcapy python-pyx
