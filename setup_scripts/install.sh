61c70cfb52b8c4bf7e0aad2150e6d6b3ed515ee8
David Karapetyan <david.karapetyan@gmail.com>
2016-07-13 19:16:34 -0400

    added htop to list of requirements

--- setup_scripts/install.sh
+++ setup_scripts/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get install python-pip gnuplot-x11 python-scapy python-pcapy python-pyx tcpdump tmux git zsh python-dev python-pandas ipython python-ipdb
+sudo apt-get install python-pip gnuplot-x11 python-scapy python-pcapy python-pyx tcpdump tmux git zsh python-dev python-pandas ipython python-ipdb htop
 sudo chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
