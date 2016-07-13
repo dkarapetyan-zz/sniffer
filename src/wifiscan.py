f911e6b0a2925b9b57f74031b545107eaad0e81c
David Karapetyan <david.karapetyan@gmail.com>
2016-07-12 23:09:44 -0400

    bugfixes--using globals now to run. Finally have output

--- src/wifiscan.py
+++ src/wifiscan.py
@@ -1,17 +1,18 @@
 import datetime
+import logging.config
 import threading
 
 import pandas as pd
+from numpy import logical_and
 from scapy.layers.dot11 import Dot11
 from scapy.sendrecv import sniff
+
+from logging_config import lcfg
 
 PROBE_REQUEST_TYPE = 0
 PROBE_REQUEST_SUBTYPE = 4
 base_df = pd.DataFrame()
 occupancy_series = pd.Series()
-
-import logging.config
-from logging_config import lcfg
 
 logging.config.dictConfig(lcfg)
 logger = logging.getLogger()
@@ -23,19 +24,21 @@ def packet_handler(pkt):
         if pkt.type == PROBE_REQUEST_TYPE and pkt.subtype == \
                 PROBE_REQUEST_SUBTYPE:
             new_info_df = pd.DataFrame(
-                data={'addr': pkt.addr2, 'info': pkt.info},
-                index=[now])
-            base_df.append(new_info_df)
+                data={'addr': pkt.addr2, 'info': pkt.info}, index=[now])
+            global base_df
+            base_df = base_df.append(new_info_df)
             new_occ_ts = pd.Series(data=occupancy_counter(base_df), index=[now])
-            occupancy_series.append(new_occ_ts)
+            global occupancy_series
+            occupancy_series = occupancy_series.append(new_occ_ts)
             logging.info("AP MAC: %s with SSID: %s " % (pkt.addr2, pkt.info))
 
 
 def occupancy_counter(df=pd.DataFrame()):
     now = datetime.datetime.utcnow()
     past = now - datetime.timedelta(minutes=15)
-    df_subset = df.between_time(start_time=now, end_time=past)
-    df_subset = df_subset.drop_duplicates(df_subset, keep='last')
+    range = logical_and(df.index >= past, df.index <= now)
+    df_subset = df[range]
+    df_subset = df_subset.drop_duplicates()
     return len(df_subset)
 
 
@@ -46,5 +49,6 @@ def things_to_be_written():
 
 def main(the_device):
     logging.info("Starting scan")
-    threading.Timer(100, things_to_be_written)
+    timer = threading.Timer(100, things_to_be_written)
+    timer.start()
     sniff(iface=the_device, prn=packet_handler)
