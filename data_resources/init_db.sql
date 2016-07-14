986e2472cc1c4415034f1aee3376c10d7f09c42f
David Karapetyan <david.karapetyan@gmail.com>
2016-07-14 14:32:55 -0400

    working ddl file

--- data_resources/init_db.sql
+++ data_resources/init_db.sql
@@ -1,33 +1,26 @@
-CREATE ROLE pi
-NOSUPERUSER INHERIT CREATEROLE;
+CREATE USER pi
+WITH PASSWORD NULL;
 
-CREATE ROLE pi LOGIN
-PASSWORD '';
-
-
-CREATE DATABASE pi
+CREATE DATABASE west_end_646
 WITH OWNER = "pi"
-ENCODING = 'UTF8'
-TABLESPACE = pg_default
-LC_COLLATE = 'English_United States.1252'
-LC_CTYPE = 'English_United States.1252'
-CONNECTION LIMIT = -1;
+ENCODING = 'UTF8';
+
+-- GRANT ALL PRIVILEGES ON DATABASE west_end_646 TO pi;
+
+\connect west_end_646;
 
 CREATE SCHEMA occupancy_schema
-  AUTHORIZATION "pi";
-
-
-CREATE TABLE occupancy
-(
-  date      DATE NOT NULL,
-  occupancy INT
-);
-
-CREATE TABLE all_info
-(
-  date DATE NOT NULL,
-  mac  CHAR(17),
-  ssid VARCHAR(64)
-);
+  AUTHORIZATION "pi"
+  CREATE TABLE occupancy
+  (
+    date      DATE NOT NULL,
+    occupancy INT
+  )
+  CREATE TABLE all_info
+  (
+    date DATE NOT NULL,
+    mac  CHAR(17),
+    ssid VARCHAR(64)
+  );
 
 
