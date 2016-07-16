3c8692bc8eb22f7c56bad0d00a43ce74ce2c0c44
David Karapetyan <david.karapetyan@gmail.com>
2016-07-15 23:15:18 -0400

    beginning of extracting vals to dbconfig

--- src/model_config.py
+++ src/model_config.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-
+# import os
 
 class ModelConfig:
     def __init__(self):
@@ -11,3 +11,18 @@ class ModelConfig:
     nary_thresh = 5
     accuracy = 1  # in minutes
     gap_threshold = 2
+
+
+    # class DBConfig:
+    #     def __init__(self):
+    #         pass
+    #
+    #     url = 'postgresql://postgres@74.71.229.106:5432/'
+    #     user = 'postgres'
+    #     username = os.environ.get('DB_USERNAME')
+    #     password = os.environ.get('DB_PASSWORD')
+    #     weather_db_name = 'weather'
+    #     weather_history_collection_name = 'history'
+    #     weather_forecast_collection_name = 'forecast'
+    #     building_db_name = 'skynet'
+    #     building_collection_name = 'timeseries'
